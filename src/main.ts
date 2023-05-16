dcaacf396facedc59ecfe4427a7fb9a05df7646f
Denis <71621555+mrklv01@users.noreply.github.com>
2023-05-16 23:19:43 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app)
   app.enableCors()
   app.setGlobalPrefix('api')
-  await app.listen(8080);
+  await app.listen(process.env.PORT||8080);
 }
 bootstrap();
